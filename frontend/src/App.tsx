8ed0a4b7c1fec3899cb4d58b64ff85b65a2f8d28
Adrian Bogarill <adgo@lottoland.com>
2023-03-15 16:41:56 +0100

    data table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,35 @@
+import "primereact/resources/themes/lara-light-purple/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import React, {useEffect, useState} from 'react';
 import logo from './logo.png';
 import './App.css';
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
+import {FilterMatchMode} from "primereact/api";
+import {InputText} from "primereact/inputtext";
 
-type resultProps = {
-    id: string;
-    name: string;
-    phone: string;
-    petName: string;
-    petType: string;
-    allergy: string;
-    notes: string;
+type ResultProps = {
+    id: string,
+    name: string,
+    phone: string,
+    petName: string,
+    petType: string,
+    allergy: string,
+    notes: string
 };
 
 function App() {
-    const [result, setResult] = useState<resultProps[]>([]);
+    const [result, setResult] = useState<Array<ResultProps>>([]);
+    const [loading, setLoading] = useState(true);
+    const [globalFilterValue, setGlobalFilterValue] = useState('');
+    const [filters, setFilters] = useState({
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        petName: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        phone: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        notes: { value: null, matchMode: FilterMatchMode.CONTAINS }
+    });
 
     useEffect(() => {
         const api = async () => {
@@ -23,28 +39,46 @@ function App() {
             const jsonData = await data.json();
             setResult(jsonData);
         };
-
-        api();
+        api().then(r => setLoading(false));
     }, []);
+
+    const onGlobalFilterChange = (e: any) => {
+        const value = e.target.value;
+        let _filters = { ...filters };
+
+        _filters['global'].value = value;
+
+        setFilters(_filters);
+        setGlobalFilterValue(value);
+    };
+
+    const renderHeader = () => {
+        return (
+            <div className="flex justify-content-end">
+                <span className="p-input-icon-left">
+                    <i className="pi pi-search" />
+                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Buscar todos" />
+                </span>
+            </div>
+        );
+    };
+
+    const header = renderHeader();
 
     return (
         <div className="App">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h2>Clients</h2>
+            <img src={logo} className="App-logo" alt="logo" />
             <div className="App-intro">
-                {result.map((value) => {
-                    return (
-                        <div>
-                            <div>{value.id}</div>
-                            <div>{value.name}</div>
-                            <div>{value.phone}</div>
-                            <div>{value.petName}</div>
-                            <div>{value.petType}</div>
-                            <div>{value.allergy}</div>
-                            <div>{value.notes}</div>
-                        </div>
-                    );
-                })}
+                <DataTable value={result} paginator rows={10} dataKey="id" filters={filters} filterDisplay="row"
+                           loading={loading} globalFilterFields={['name', 'phone', 'petName', 'notes']} header={header}
+                           emptyMessage="No hay clientes todavia">
+                    <Column field="name" header="Nombre cliente" filter filterPlaceholder="Buscar por nombre" sortable/>
+                    <Column field="phone" header="Telefono" filter filterPlaceholder="Buscar por telefono"/>
+                    <Column field="petName" header="Nombre mascota" filter filterPlaceholder="Buscar por mascota" sortable/>
+                    <Column field="petType" header="Tipo de mascota"/>
+                    <Column field="allergy" header="Alergias"/>
+                    <Column field="notes" header="Notas" filter filterPlaceholder="Buscar por notas" />
+                </DataTable>
             </div>
         </div>
     );
